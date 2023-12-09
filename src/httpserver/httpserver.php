e2b4752030bb1c14e389a593e99e4314f531f66f
pascualmg <pashkiter@gmail.com>
2023-12-09 20:18:12 +0100

    middleware in a socket

--- src/httpserver/httpserver.php
+++ src/httpserver/httpserver.php
@@ -6,10 +6,25 @@ use Passh\Rx\httpserver\FilePostRepository;
 use React\EventLoop\Loop;
 use React\Http\HttpServer;
 use React\Http\Message\Response;
+use React\Socket\SocketServer;
 
 $loop = Loop::get();
 
 $httpServer = new HttpServer(function (\Psr\Http\Message\ServerRequestInterface $request) use ($loop) {
+
+    $jsonResponse = function ($code, $body) {
+        return new Response(
+            $code,
+            ['Content-Type' => 'application/json'],
+            json_encode($body)
+        );
+    };
+
+
+    if ($request->getMethod() === 'POST') {
+        $bodyContents = json_decode($request->getBody()->getContents(), true);
+        $bufferedBody = $request->getBody();
+    }
     $getParamOrNull = function (string $maybeParam, array $queryParams): ?string {
         return $queryParams()[$maybeParam] ?? null;
     };
@@ -20,18 +35,20 @@ $httpServer = new HttpServer(function (\Psr\Http\Message\ServerRequestInterface 
     $allPosts = $postRepository->findAll();
 
 
-    return new Response(
-        200,
-        ['Content-Type' => 'application/json'],
-        json_encode($allPosts)
-    );
+    return $jsonResponse(200, $allPosts);
+
 });
 
-$port8000 = new \React\Socket\SocketServer(
+$port8000 = new SocketServer(
     '127.0.0.1:8000',
     [],
     $loop
 );
+
+
+$port8000->on('connection', function (\React\Socket\ConnectionInterface $connection) {
+    $connection->on('data', 'var_dump');
+});
 
 $httpServer->listen($port8000);
 
