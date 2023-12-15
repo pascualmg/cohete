42c05f922dae276231b073c25ccf6cc1418dedf7
pascualmg <pashkiter@gmail.com>
2023-12-15 11:36:27 +0100

    Refactor handle method in TestController
    The handle method in TestController has been refactored for improved clarity and ease of understanding. The GET and POST request handling logic have been removed and replaced with code executing MysqlPostRepository's findAll method and returning a response including both payload and client IP info. Superfluous namespaces have also been deleted to clean up the code.

--- src/ddd/Infrastructure/RequestHandler/TestController.php
+++ src/ddd/Infrastructure/RequestHandler/TestController.php
@@ -2,52 +2,38 @@
 
 namespace Pascualmg\Rx\ddd\Infrastructure\RequestHandler;
 
-use Pascualmg\Rx\ddd\Infrastructure\Repository\Post\FilePostRepository;
 use Pascualmg\Rx\ddd\Infrastructure\Repository\Post\MysqlPostRepository;
 use Psr\Http\Message\ServerRequestInterface;
 use React\Http\Message\Response;
 use React\Promise\Deferred;
+use React\Promise\PromiseInterface;
 
 class TestController
 {
-    public function handle(ServerRequestInterface $request): \React\Promise\PromiseInterface
+    public function handle(ServerRequestInterface $request): PromiseInterface
     {
         $deferred = new Deferred();
 
-        if ($request->getMethod() === 'GET') {
-            $deferred->resolve(
-                new Response(
-                    200,
-                    ['Content-Type' => 'application/json'],
-                    json_encode(['client_ip' => $request->getAttribute('client-ip')], JSON_THROW_ON_ERROR)
-                )
-            );
-        }
-
-
-        if ($request->getMethod() === 'POST') {
-            [$id, $fecha] = array_values(
-                json_decode($request->getBody()->getContents(), true, 2, JSON_THROW_ON_ERROR)
-            );
-
-            $deferred->resolve(
-                (new MysqlPostRepository())->findAll()
-                    ->then(function (array $posts) {
-                        return new Response(
-                            200,
-                            ['Content-Type' => 'application/json'],
-                            json_encode(['payload' => $posts], JSON_THROW_ON_ERROR)
-                        );
-                    })
-                    ->catch(function (\Throwable $error) {
-                        return new Response(
-                            500,
-                            ['Content-Type' => 'application/json'],
-                            json_encode(['error' => $error->getMessage()], JSON_THROW_ON_ERROR)
-                        );
-                    })
-            );
-        }
+        $deferred->resolve(
+            (new MysqlPostRepository())->findAll()
+                ->then(function (array $posts) use ($request) {
+                    return new Response(
+                        200,
+                        ['Content-Type' => 'application/json'],
+                        json_encode([
+                            'payload' => $posts,
+                            'your_ip' => $request->getAttribute('client_ip')
+                        ], JSON_THROW_ON_ERROR)
+                    );
+                })
+                ->catch(function (\Throwable $error) {
+                    return new Response(
+                        500,
+                        ['Content-Type' => 'application/json'],
+                        json_encode(['error' => $error->getMessage()], JSON_THROW_ON_ERROR)
+                    );
+                })
+        );
 
         return $deferred->promise();
     }
