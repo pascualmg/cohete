024c6c1e76fb667d2f92a5afd3c1e8d3daba8ff9
pascualmg <pashkiter@gmail.com>
2023-12-18 02:52:34 +0100

    Update WebSocket server and add new route
    This commit modifies WebSocket server error reporting settings and refactors some methods for better clarity. It also introduces a new WebSocket route in the HttpServer routes, and a new HtmlRequestHandler, improving route handling and streamlining server response generation.

---
+++ src/ddd/Infrastructure/RequestHandler/HtmlRequestHandler.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Pascualmg\Rx\ddd\Infrastructure\RequestHandler;
+
+use Fig\Http\Message\StatusCodeInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use React\Http\Message\Response;
+use React\Promise\PromiseInterface;
+use React\Stream\ReadableResourceStream;
+use React\Stream\ThroughStream;
+
+class HtmlRequestHandler implements Handler
+{
+
+    public function handle(ServerRequestInterface $_): ResponseInterface|PromiseInterface
+    {
+        $foo = new ThroughStream('strtoupper');
+
+        //en vez de usar un file_get_contents usamos un stream
+        $html = new ReadableResourceStream(
+            fopen(
+                __DIR__ . '/../../../scripts/Drafts/websocketServer/websocketTest.html',
+                'rb'
+            )
+        );
+
+
+        return new Response(
+            StatusCodeInterface::STATUS_OK,
+            ['Content-Type' => 'text/html'],
+            $html->pipe($foo)
+        );
+    }
+}
--- src/scripts/Drafts/websocketServer/wsServer.php
+++ src/scripts/Drafts/websocketServer/wsServer.php
@@ -2,7 +2,7 @@
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED);
 require "../../../../vendor/autoload.php";
 
 use Pascualmg\Rx\ddd\Infrastructure\Repository\Post\MysqlPostRepository;
@@ -10,35 +10,39 @@ use Ratchet\ConnectionInterface;
 use Ratchet\MessageComponentInterface;
 use Ratchet\Server\IoServer;
 use Ratchet\WebSocket\WsServer;
+use React\EventLoop\Loop;
 
-$messageComponent = new class () implements MessageComponentInterface {
-    public function onOpen(ConnectionInterface $conn)
+$messageComponent = new class() implements MessageComponentInterface {
+    public function onOpen(ConnectionInterface $conn): void
     {
-        var_dump('open');
+        echo 'conexion entrante';
+        var_dump($conn);
     }
-
-    public function onClose(ConnectionInterface $conn)
+    public function onClose(ConnectionInterface $conn): void
     {
-
         var_dump('close');
     }
 
-    public function onError(ConnectionInterface $conn, \Exception $e)
+    public function onError(ConnectionInterface $conn, \Exception $e): void
     {
         var_dump($e);
     }
 
-    public function onMessage(ConnectionInterface $from, $msg)
+    public function onMessage(ConnectionInterface $from, $msg): void
     {
-        (new MysqlPostRepository())->findAll()->then(
-            function ($result) use ($from) {
-                foreach ($result as $post) {
-                    $from->send((string)$post);
-                }
-
-
-            }
-        );
+        (new MysqlPostRepository())
+            ->findAll()
+            ->then(function (array $result) use ($from) {
+                $loop = Loop::get();
+                $sendPostsTimer = $loop->addPeriodicTimer(1, function () use ($from, $result) {
+                    foreach ($result as $post) {
+                        $from->send((string)$post);
+                    }
+                });
+                $loop->addTimer(10, function () use ($sendPostsTimer, $loop) {
+                    $loop->cancelTimer($sendPostsTimer);
+                });
+            });
     }
 };
 $wsServer = new WsServer($messageComponent);
