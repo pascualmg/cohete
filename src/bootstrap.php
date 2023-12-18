d99242dfedd66bcb0e55737a0651959d099a8a49
pascualmg <pashkiter@gmail.com>
2023-12-18 02:49:21 +0100

    Enable error reporting in bootstrap file
    Added lines in bootstrap.php to enable error reporting. These changes include displaying errors and startup errors, and reporting all php errors which will ease debugging during the development process.

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 require __DIR__ . '/../vendor/autoload.php';
 use Pascualmg\Rx\ddd\Infrastructure\HttpServer\ReactHttpServer;
