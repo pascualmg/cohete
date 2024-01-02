eecee607c53f3e32895e94415b8ee49840ca3841
pascualmg <pascual.munoz@pccomponentes.com>
2024-01-02 11:38:53 +0100

    seeder for post usin fake

---
+++ src/ddd/Infrastructure/db/seeds/PostSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+use Phinx\Seed\AbstractSeed;
+
+class PostSeeder extends AbstractSeed
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeders is available here:
+     * https://book.cakephp.org/phinx/0/en/seeding.html
+     */
+    public function run(): void
+    {
+        $faker = Faker\Factory::create();
+        $postFakeData = [];
+
+        for($i = 0; $i < 1000; $i++) {
+            $postFakeData[] = [
+                'headline' => $faker->sentence(),
+                'articleBody' => $faker->paragraph(),
+                'author' => $faker->name(),
+                'image' => $faker->imageUrl(),
+                'datePublished' => $faker->date(),
+            ];
+        }
+
+        $this->insert('post', $postFakeData);
+    }
+}
