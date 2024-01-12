3286cdae76f86f1974c9c89ed9536da5d60b0aed
pascualmg <pashkiter@gmail.com>
2024-01-12 22:38:42 +0100

    Revamp chat-box design and enhance messaging functionality
    The `chat-box.js` file underwent major changes for a more intuitive user experience and improved aesthetics. Redundant chat-box instances were removed, and a function `findByUuid` was deleted from `ConnectionPool.php` to increase server efficiency. Particularly, CSS modifications were implemented to enhance the chat box layout, and a keypress event listener was added for enabling message sending with the 'Enter' key.

--- src/ddd/Infrastructure/HttpServer/html/chat-box.js
+++ src/ddd/Infrastructure/HttpServer/html/chat-box.js
@@ -11,27 +11,66 @@ class ChatBoxComponent extends HTMLElement {
 
     render() {
         this.shadowRoot.innerHTML = `
-             <style>
-                #messageInput { width: 400px; }
-                #chat-box {
-                    color: greenyellow;
-
-                }
-
-              </style>
-              <div id="chat-box"></div>
-              <div id="userInputSection">
-
-              <label for="messageInput"></label>
-              <input id="messageInput" type="text" placeholder="Write a message">
-              <button id="sendButton">Send</button>
-              </div> `;
+    <style>
+    #chat-container {
+        max-width: 400px;
+        margin: auto;
+        padding: 20px; 
+        background-color: #002b36; 
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.5); 
+        border-radius: 10px;
+    }
+    #messageInput { 
+        width: 80%;
+        padding: 10px;
+        border-radius: 5px;
+        border: 1px solid #073642;
+        font-size: 1em;
+        margin-bottom: 10px;
+        color: #586e75;
+        background-color: #002b36;
+    }
+    #chat-box {
+        color: #859900;
+        max-height: 400px;
+        overflow-y: auto;
+        border: 1px solid #073642;
+        border-radius: 10px;
+        padding: 20px;
+        background-color: #002b36;
+        margin-bottom: 10px;
+    }
+    #userInputSection {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+    #sendButton {
+        font-size: 1em;
+        padding: 10px;
+        border: none;
+        border-radius: 5px;
+        color: #002b36;
+        background-color: #859900;
+        cursor: pointer;
+    }
+    #sendButton:hover {
+        background-color: #586e75;
+    }
+    </style>
+    <div id="chat-container">
+      <div id="chat-box"></div>
+      <div id="userInputSection">
+        <input id="messageInput" type="text" placeholder="Write a message">
+        <button id="sendButton">Send</button>
+      </div>
+    </div>`;
     }
 
     initWebSocket() {
         const chatbox = this.shadowRoot.querySelector('#chat-box');
         const messageInput = this.shadowRoot.querySelector('#messageInput');
-        const sendButton = this.shadowRoot.querySelector('#sendButton'); // Replace with your server's WebSocket URL
+        const sendButton = this.shadowRoot.querySelector('#sendButton');
         const websocket = new WebSocket('ws://localhost:8001');
 
         websocket.onopen = () => {
@@ -43,7 +82,7 @@ class ChatBoxComponent extends HTMLElement {
             const payload = JSON.parse(message.data)
             console.log(payload);
             const div = document.createElement('div');
-            div.textContent = message.data;
+            div.textContent = payload.msg || ""
             chatbox.appendChild(div);
         };
 
@@ -54,7 +93,17 @@ class ChatBoxComponent extends HTMLElement {
                 messageInput.value = '';
             }
         });
+        messageInput.addEventListener('keypress', function (e) {
+            if (e.key === 'Enter' && this.value.trim() !== '') {
+                const message = messageInput.value.trim();
+                websocket.send(message)
+                messageInput.value = '';
+            }
+        })
     }
 }
 
-window.customElements.define('chat-box', ChatBoxComponent);
+window
+    .customElements
+    .define('chat-box', ChatBoxComponent)
+;
