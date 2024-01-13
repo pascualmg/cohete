d4f1da3e00fcb2529068e8c21082c549336672dc
pascualmg <pashkiter@gmail.com>
2024-01-13 01:00:46 +0100

    Update styling and functionality in chat-box.js
    The chat-box.js file has been updated to redesign the chat box interface and also increase its functionality. The styles for #chat-container, #messageInput, #chat-box, #userInputSection, and other related elements have been altered. In addition, the WebSocket server's IP address has been changed and message sending mechanism has been modified to only trigger on pressing 'Enter'.

--- src/ddd/Infrastructure/HttpServer/html/chat-box.js
+++ src/ddd/Infrastructure/HttpServer/html/chat-box.js
@@ -11,67 +11,61 @@ class ChatBoxComponent extends HTMLElement {
 
     render() {
         this.shadowRoot.innerHTML = `
-    <style>
+<style>
     #chat-container {
-        max-width: 400px;
-        margin: auto;
-        padding: 20px; 
-        background-color: #002b36; 
-        box-shadow: 0 0 10px rgba(0, 0, 0, 0.5); 
-        border-radius: 10px;
-    }
-    #messageInput { 
-        width: 80%;
-        padding: 10px;
-        border-radius: 5px;
-        border: 1px solid #073642;
-        font-size: 1em;
-        margin-bottom: 10px;
-        color: #586e75;
-        background-color: #002b36;
-    }
-    #chat-box {
-        color: #859900;
-        max-height: 400px;
-        overflow-y: auto;
-        border: 1px solid #073642;
-        border-radius: 10px;
-        padding: 20px;
-        background-color: #002b36;
-        margin-bottom: 10px;
-    }
-    #userInputSection {
+        position: fixed;
+        bottom: 0;
+        height: 50vh;
+        width: 100%;
         display: flex;
-        justify-content: space-between;
-        align-items: center;
-    }
-    #sendButton {
-        font-size: 1em;
-        padding: 10px;
-        border: none;
+        flex-direction: column;
+        padding: 5px;
+        background-color: #073642;
+        color: #93a1a1;
         border-radius: 5px;
-        color: #002b36;
-        background-color: #859900;
-        cursor: pointer;
+        border: 1px solid #586e75;
     }
-    #sendButton:hover {
-        background-color: #586e75;
+
+    #chat-box {
+        overflow-y: auto;
+        flex-grow: 1;
     }
-    </style>
+#chat-box div {
+    border: 1px solid #586e75;
+    border-radius: 10px;
+    padding: 10px;
+    margin: 2px 0; /* Añade un pequeño margen vertical entre los mensajes */
+    background-color: #073642;
+    color: #93a1a1;
+}
+    #userInputSection {
+        margin-top: 10px;
+    }
+
+    #messageInput {
+        width: 100%;
+        padding: 5px;
+        border-radius: 5px;
+        border: none;
+        background-color: #002b36;
+        color: #839496;
+        font-size: larger;
+    }
+</style>
     <div id="chat-container">
-      <div id="chat-box"></div>
-      <div id="userInputSection">
-        <input id="messageInput" type="text" placeholder="Write a message">
-        <button id="sendButton">Send</button>
-      </div>
-    </div>`;
+        <div id="chat-box"></div>
+        <div id="userInputSection">
+            <input id="messageInput" type="text" placeholder="Write a message">
+        </div>
+    </div>
+    `;
     }
 
     initWebSocket() {
         const chatbox = this.shadowRoot.querySelector('#chat-box');
         const messageInput = this.shadowRoot.querySelector('#messageInput');
         const sendButton = this.shadowRoot.querySelector('#sendButton');
-        const websocket = new WebSocket('ws://localhost:8001');
+        const websocket = new WebSocket('ws://192.168.2.119:8001');
 
         websocket.onopen = () => {
             console.log('WebSocket Client Connected');
@@ -84,18 +78,13 @@ class ChatBoxComponent extends HTMLElement {
             const div = document.createElement('div');
             div.textContent = payload.msg || ""
             chatbox.appendChild(div);
+            chatbox.scrollTop = chatbox.scrollHeight
         };
 
-        sendButton.addEventListener('click', () => {
-            const message = messageInput.value.trim();
-            if (message !== "") {
-                websocket.send(message);
-                messageInput.value = '';
-            }
-        });
         messageInput.addEventListener('keypress', function (e) {
             if (e.key === 'Enter' && this.value.trim() !== '') {
                 const message = messageInput.value.trim();
+
                 websocket.send(message)
                 messageInput.value = '';
             }
