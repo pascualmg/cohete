3bb9c0f8f69d97afea5e1bd6793ed52d7613beed
pascualmg <pashkiter@gmail.com>
2024-01-21 02:11:42 +0100

    Update RabbitMQ connection and implement EventLoopScheduler
    The RabbitMQ connection setup process is refactored in the script. Additionally, EventLoopScheduler from RxPHP is now being utilized to set a default scheduler. These changes are aimed to enhance dispatch accuracy and optimize interaction with the RabbitMQ server.

--- src/ddd/Infrastructure/scripts/Drafts/try_to_send_something_to_rabbit_mq.php
+++ src/ddd/Infrastructure/scripts/Drafts/try_to_send_something_to_rabbit_mq.php
@@ -6,18 +6,27 @@ use Bunny\Async\Client;
 use pascualmg\reactor\ddd\Domain\Bus\Message;
 use pascualmg\reactor\ddd\Infrastructure\Bus\BunnieMessageBus;
 use React\EventLoop\Loop;
+use Rx\Scheduler;
+use Rx\Scheduler\EventLoopScheduler;
 
 
 $loop = Loop::get();
+$client = new Client($loop, [
+    'host' => '127.0.0.1', // host to connect to
+    'port' => 5672, // port to connect to
+    'vhost' => '/', // the virtual host to use
+    'user' => 'rabbitmq', // user
+    'password' => 'rabbitmq' // password
+]);
+
+
+
 $bunnieMb = new  BunnieMessageBus(
-    new Client($loop, [
-        'host' => '127.0.0.1', // host to connect to
-        'port' => 5672, // port to connect to
-        'vhost' => '/', // the virtual host to use
-        'user' => 'rabbitmq', // user
-        'password' => 'rabbitmq' // password
-    ])
+    $client
 );
+
+$scheduler = new EventLoopScheduler($loop);
+Scheduler::setDefaultFactory(static fn () => $scheduler);
 
 $bunnieMb->dispatch(
    new Message('foo', ['bar'])
