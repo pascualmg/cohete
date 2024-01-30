01cc0318f7749f5d0ab876c688d3ad80b6a3f37b
pascualmg <pascual.munoz@pccomponentes.com>
2024-01-30 12:06:30 +0100

    Add check and removal option for bashrc backup in nix script
    A check has been implemented for the existence of a /etc/bash.bashrc.backup-before-nix file during the Nix installation process. If present, the script now offers an option to delete this backup file. This change aims to clean up potential lingering backup files and prevent an accidental execution of an outdated bashrc file.

--- src/ddd/Infrastructure/scripts/install-nix.sh
+++ src/ddd/Infrastructure/scripts/install-nix.sh
@@ -11,6 +11,19 @@ if command -v nix >/dev/null 2>&1
 then
     echo "Nix ya está instalado. No se realizará ninguna acción."
 else
+  if [ -f /etc/bash.bashrc.backup-before-nix ]
+  then
+      echo "El archivo /etc/bash.bashrc.backup-before-nix existe."
+      read -p "¿Deseas eliminar este archivo? [y/N] " respuesta
+      if [[ $respuesta =~ ^[Yy]$ ]]
+      then
+          sudo rm -f /etc/bash.bashrc.backup-before-nix
+          echo "El archivo /etc/bash.bashrc.backup-before-nix ha sido eliminado."
+      else
+          echo "Abortando la instalación de Nix."
+          exit 1
+      fi
+  fi
     echo "Nix no está instalado. Iniciando la instalación..."
   	curl -L https://nixos.org/nix/install | sh -s -- --daemon
     echo "Instalación de Nix completada. Activando características experimentales..."
