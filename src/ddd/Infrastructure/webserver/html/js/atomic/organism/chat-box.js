305288b1144668340caca4d74ec8a765ac0f6546
pascualmg <pashkiter@gmail.com>
2024-04-20 23:31:02 +0200

    Cambio de protocolo de WebSocket de 'ws' a 'wss' en chat-box.js

--- src/ddd/Infrastructure/webserver/html/js/atomic/organism/chat-box.js
+++ src/ddd/Infrastructure/webserver/html/js/atomic/organism/chat-box.js
@@ -9,7 +9,7 @@ class ChatBox extends HTMLElement {
     connectedCallback() {
         const host = this.getAttribute("host") || '0.0.0.0'
         const port = this.getAttribute("port") || '8001'
-        const uri = `ws://${host}:${port}`
+        const uri = `wss://${host}:${port}`
         const group = this.getAttribute("group") || 'general'
 
         this.render(group);
